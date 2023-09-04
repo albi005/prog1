c4bd56dca0a3e5d445da63ca975328cbb94bf193
Albert Ragány-Németh <me@alb1.hu>
2023-09-04 18:25:36 +0200

    Init

---
+++ .nvim.lua
@@ -0,0 +1,22 @@
+local dap = require('dap')
+
+dap.configurations.c = {
+    {
+        name = "Run C program",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            vim.cmd('!gcc ' .. vim.fn.expand('%') .. ' -o bin/a -g')
+            return 'bin/a'
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = false,
+        setupCommands = {
+            {
+                text = '-enable-pretty-printing',
+                description =  'enable pretty printing',
+                ignoreFailures = false
+            },
+        },
+    },
+}

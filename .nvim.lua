06950ed08a928b0f74dc4e1938ace95012eaf981
Albert Ragány-Németh <me@alb1.hu>
2023-10-31 20:54:38 +0100

    Reorganize files

--- .nvim.lua
+++ .nvim.lua
@@ -29,6 +29,19 @@ dap.adapters.cppdbg = {
     command = os.getenv'OPEN_DEBUG_PATH',
 }
 
-vim.keymap.set('n', '<Leader>r', ':!gcc ' .. options .. ' "%" && ./a.out < "%.in"<CR>', { noremap = true });
-vim.keymap.set('n', '<Leader>t', ':!gcc ' .. options .. ' "%" && ./a.out <CR>', { noremap = true });
+-- use %.in if it exists
+vim.keymap.set('n', '<Leader>r', function ()
+    local input = vim.fn.expand('%:r') .. '.in'
+    local exists = vim.fn.filereadable(input)
+    if exists then
+        vim.cmd('!gcc ' .. options .. ' "%" && ./a.out < "' .. input .. '"')
+    else
+        vim.cmd('!gcc ' .. options .. ' "%" && ./a.out')
+    end
+end)
+
+-- compile and run in an interactive shell
+-- <Leader>t
+vim.keymap.set('n', '<Leader>t', ':!gcc ' .. options .. ' "%" && ./a.out |& tee /dev/tty<CR>', { noremap = true });
+
 vim.keymap.set('n', '<Leader>s', ':!gcc ' .. options .. ' "%" `sdl2-config --cflags --libs` -lSDL2_gfx -lSDL2_ttf -lSDL2_image -lSDL2_mixer && ./a.out <CR>', { noremap = true });
--- ea04/some.c
+++ ea04/some.c
@@ -4,6 +4,9 @@ int main() {
     char str[] = "Hello";
     str[5] = '!';
     str[6] = 'a';
+
+    do {
+    }
     
     printf("%s\n", str);
 }
